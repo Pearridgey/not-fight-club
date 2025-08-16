343cb41618a96401a0ad4b86f7bc9408bc288119
unknown <pearridgey@gmail.com>
2025-08-16 14:25:22 +0300

    fix: username

---
+++ scripts/battle.js
@@ -0,0 +1,25 @@
+let Player = {
+    name: 
+}
+
+
+
+
+
+let Enemies = [
+{
+    name: "Trump",
+    health: 80,
+    attackPwr: 35,
+    attackChoice: null,
+    defenseChoice: null,
+    
+},
+{
+    name: "von der Leyen",
+    health: 95,
+    attackPwr: 28,
+    attackChoice: null,
+    defenseChoice: null,
+}
+]
